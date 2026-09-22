9bb39d16442b26d72bd7516795c1670438b6b498
Diana qian <3440108395@qq.com>
2026-09-22 10:19:40 +0800

    Delete macos directory

--- macos/Runner/PlatformChannelNames.swift
+++
@@ -1,9 +0,0 @@
-import FlutterMacOS
-
-enum OronBoxChannelNames {
-  static let zepposAppSettings = "oronbox/zeppos_app_settings"
-  static let miAccountTwoFactor = "oronbox/mi_account_2fa"
-  static let classicSpp = "oronbox/classic_spp"
-  static let classicSppEvents = "oronbox/classic_spp/events"
-  static let classicSppScanEvents = "oronbox/classic_spp/scan_events"
-}
