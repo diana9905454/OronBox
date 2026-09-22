b215503f57772a94c329419c59c24499bc59883f
Diana qian <3440108395@qq.com>
2026-09-22 10:19:27 +0800

    Delete android directory

--- android/app/src/main/kotlin/org/zxor/oronbox/ApkFileProvider.kt
+++
@@ -1,6 +0,0 @@
-package org.zxor.oronbox
-
-import androidx.core.content.FileProvider
-
-/** Exposes downloaded APKs (in filesDir/apk) to the system installer. */
-class ApkFileProvider : FileProvider()
